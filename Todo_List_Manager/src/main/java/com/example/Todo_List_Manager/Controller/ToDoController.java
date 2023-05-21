a05935238de393f1ecf267988aea7087ec8d4def
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-05-21 05:48:38 -0700

    complete Todo Retrieval (All todos):

--- Todo_List_Manager/src/main/java/com/example/Todo_List_Manager/Controller/ToDoController.java
+++ Todo_List_Manager/src/main/java/com/example/Todo_List_Manager/Controller/ToDoController.java
@@ -8,11 +8,13 @@ import com.example.Todo_List_Manager.Service.ToDoService;
 import com.example.Todo_List_Manager.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class ToDoController {
@@ -38,5 +40,9 @@ public class ToDoController {
         todo.setCreatedDate(new Date());
         toDoService.addItems(todo);
 
+    }
+    @GetMapping("api/todos")
+    public List<Todo> getTodos () {
+        return toDoService.getTodo();
     }
 }
--- Todo_List_Manager/src/main/java/com/example/Todo_List_Manager/Service/ToDoService.java
+++ Todo_List_Manager/src/main/java/com/example/Todo_List_Manager/Service/ToDoService.java
@@ -7,11 +7,16 @@ import com.example.Todo_List_Manager.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ToDoService {
     @Autowired
     ToDoRepository toDoRepository;
     public void addItems(Todo todo) {
         toDoRepository.save(todo);
+    }
+    public List<Todo> getTodo(){
+        return toDoRepository.findAll();
     }
 }
