6a08c51bf43458fb5b5d5e0f0118ebd507646805
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-05-24 05:48:20 -0700

    Remove Bugs from the code and test for 3rd scenario (Todo Retrieval (All todos))

--- Todo_List_Manager-main/Todo_List_Manager/AutoCommitScript.bat
+++ Todo_List_Manager-main/Todo_List_Manager/AutoCommitScript.bat
@@ -6,7 +6,7 @@ cd C:\Users\Lenovo\Documents\GitHub\Todo_List_Manager
 git add .
 
 # Commit the changes
-git commit -m "Remove Bugs from the code and test for Second scenario (Todo Creation) " --allow-empty
+git commit -m "Remove Bugs from the code and test for 3rd scenario (Todo Retrieval (All todos)) " --allow-empty
 
 # Push the changes to GitHub
 git push
