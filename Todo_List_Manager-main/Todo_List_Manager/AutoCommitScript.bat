d55176dcfd13d7ddaeb3b11a202d3af6a3c9cd4a
jqm0 <121156533+jqm0@users.noreply.github.com>
2023-05-24 05:49:41 -0700

    Remove Bugs from the code and test for 6th scenario (Todo Deletion)

--- Todo_List_Manager-main/Todo_List_Manager/AutoCommitScript.bat
+++ Todo_List_Manager-main/Todo_List_Manager/AutoCommitScript.bat
@@ -6,7 +6,7 @@ cd C:\Users\Lenovo\Documents\GitHub\Todo_List_Manager
 git add .
 
 # Commit the changes
-git commit -m "Remove Bugs from the code and test for 5th scenario (Todo Update) " --allow-empty
+git commit -m "Remove Bugs from the code and test for 6th scenario (Todo Deletion) " --allow-empty
 
 # Push the changes to GitHub
 git push
